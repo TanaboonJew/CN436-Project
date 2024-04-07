f1fc94da68865f05c4e6a41979a29566e98bba34
Chalisa Thummaraj <6410685041@student.tu.ac.th>
2024-04-07 23:12:56 +0700

    Start Project

---
+++ FitHub/FitHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitHub
+//
+//  Created by ชลิศา ธรรมราช on 7/4/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitHub/FitHub/FitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitHubApp.swift
+//  FitHub
+//
+//  Created by ชลิศา ธรรมราช on 7/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct FitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
